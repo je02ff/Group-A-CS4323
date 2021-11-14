99cb9cac01caee77ea7b9d7e14fb0d864d9b397e
Jeff Roden <roden80@gmail.com>
2021-11-13 21:14:55 -0500

    server.c on port 8081

---
+++ server.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <assert.h>
+#include <semaphore.h>
+#include "readDatabaseIntoArray.h"
+#include "Roden.h"
+
+#define SERVER_PORT_NUM 8081
+#define CLIENT_BACKLOG 25
+#define MSG_BUFFER_SIZE 10240
+/*---TCP Stuff---*/
+
+void createSocket(int *sock);
+
+void bindSocket(struct sockaddr_in *sAddress, const int *sock);
+
+void listenOnSocket(const int* sock, int backLogCount);
+
+bool acceptConnection(int* clientSock, const int* hSock, struct sockaddr_in* client);
+
+void readSocket(const int* sock, char* buffer);
+
+void writeSocket(const int* sock, char* buffer);
+
+int main() {
+    /* TCP Vars */
+    int hSocket;
+    int clientSock;
+    struct sockaddr_in server = {0};
+    struct sockaddr_in client = {0};
+    char buffer[MSG_BUFFER_SIZE];
+    pid_t pid;
+
+    createSocket(&hSocket);                                             //Creating socket for server to communicate through
+    bindSocket(&server, &hSocket);                                      // binding socket to address
+    listenOnSocket(&hSocket, CLIENT_BACKLOG);                           //Listen for clients, maximum of 3 connections
+
+    /*----Forking Clients---- */
+    while (1) {
+        if (acceptConnection(&clientSock, &hSocket, &client) == true) { //Accept incoming client connection
+            pid = fork();                                               //Fork client process
+            if(pid == 0) {                                              //code for child(client) process
+                close(hSocket);
+
+                /*---Main Logic Loop: Driven by Client Commands ---*/
+                while(1) {
+                    bzero(buffer, MSG_BUFFER_SIZE);
+                    readSocket(&clientSock, buffer);
+                }
+            } else if(pid > 0) {                                        //I'm the parent, Do parent things
+                close(clientSock);                                      //close childSocket, parent uses hSocket
+                wait(NULL);                                     //reap children
+            } else {
+                printf("Fork failed!\n");
+                exit(1);
+            }
+        }
+    }
+    return 0;
+}
+
+/*---TCP Stuff---*/
+
+void createSocket(int *sock) {
+    *sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (*sock == -1) {
+        printf("Failed to create socket, exiting server\n");
+        exit(1);
+    } else {
+        printf("Socket created\n");
+    }
+}
+
+void bindSocket(struct sockaddr_in *sAddress, const int *sock) {
+    sAddress->sin_family = AF_INET;
+    sAddress->sin_addr.s_addr = htonl(INADDR_ANY);
+    sAddress->sin_port = htons(SERVER_PORT_NUM);
+    if (bind(*sock,(struct sockaddr *)&(*sAddress), sizeof(*sAddress)) == -1) {
+        printf("Failed to bind socket to port, exiting server\n");
+        exit(1);
+    } else {
+        printf("Socket bound to port\n");
+    }
+}
+
+void listenOnSocket(const int* sock, int backLogCount) {
+    if(listen(*sock, backLogCount) == -1) {
+        printf("Failed to listen, exiting server\n");
+        exit(1);
+    } else {
+        printf("Listening through socket %d on port %d\n", *sock, SERVER_PORT_NUM);
+    }
+}
+
+bool acceptConnection(int* clientSock, const int* hSock, struct sockaddr_in* client) {
+    int clientLength = sizeof(struct sockaddr_in);
+    *clientSock = accept(*hSock, (struct sockaddr *)&(*client),(socklen_t*)&clientLength);
+    if(*clientSock == -1) {
+        printf("!!! Error connecting client !!!\n");
+        return false;
+    }
+    printf("New Connection accepted...\n");
+    return true;
+}
+
+void readSocket(const int* sock, char* buffer) {
+
+    if(read(*sock, buffer, MSG_BUFFER_SIZE) == -1) {
+        printf("failed to read socket\n");
+        exit(1);
+    }
+}
+
+void writeSocket(const int* sock, char* buffer) {
+
+    if (write(*sock, buffer, MSG_BUFFER_SIZE) == -1) {
+        printf("failed to write socket\n\n");
+        exit(1);
+    }
+
+}
